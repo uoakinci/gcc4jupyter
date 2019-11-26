c2d3da291b3f9b3c4ac0e5c46c5ec1715e06fa44
uoakinci <uoakinci@gmail.com>
2019-11-26 20:59:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Uğur Özgü Akıncı',
     author_email='uoakinci@gmail.com',
-    py_modules=['gcc_plugin', 'v2.v2', 'common.helper'],
+    py_modules=['gcc_plugin', 'v1GCC', 'v2GCC', 'common.helper'],
     url='htpps://github.com/uoakinci/gcc4jupyter',
     license='LICENSE',
     description='Jupyter notebook plugin to run GCC C/C++ code. Forked from Andrei Nechaev\'s nvcc4jupyter @ https://github.com/andreinechaev/nvcc4jupyter',
