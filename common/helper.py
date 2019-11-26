e262c2fee0144e717c82f51ef9f0e34d92e8e05a
uoakinci <uoakinci@gmail.com>
2019-11-26 20:49:46 +0300

    Update helper.py

--- common/helper.py
+++ common/helper.py
@@ -2,7 +2,7 @@ import argparse
 
 
 def get_argparser():
-    parser = argparse.ArgumentParser(description='NVCCPlugin params')
+    parser = argparse.ArgumentParser(description='GCCPlugin params')
     parser.add_argument("-t", "--timeit", action='store_true',
                         help='flag to return timeit result instead of stdout')
     return parser
