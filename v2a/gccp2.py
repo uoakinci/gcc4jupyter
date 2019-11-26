08742e7a828418811a558be559cc61226017fe8c
uoakinci <uoakinci@gmail.com>
2019-11-26 23:20:16 +0300

    Update gccp2.py

--- v2a/gccp2.py
+++ v2a/gccp2.py
@@ -5,7 +5,7 @@ from IPython.core.magic import Magics, cell_magic, magics_class
 from IPython.core.magic_arguments import argument, magic_arguments, parse_argstring
 from gcommon import ghelper
 
-compiler = '/usr/bin/g++-5'
+compiler = 'g++'
 
 
 @magics_class
